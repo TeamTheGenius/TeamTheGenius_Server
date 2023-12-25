03de9128b78939781f1ba83d4b31b33dcae96474
SSung023 <hat7206@naver.com>
2023-12-25 20:48:00 +0900

    init: 프로젝트 초기 세팅
    - .yml 파일에 DB 추가 세팅 필요

---
+++ src/test/java/com/genius/todoffin/TodoffinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genius.todoffin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoffinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

27f54dbc88c2b8b23c9326e9a381b8ec05c087b3
SSung023 <50323157+SSung023@users.noreply.github.com>
2023-12-30 02:16:50 +0900

    feat: Time formatting 파일 추가

---
+++ src/main/java/com/genius/todoffin/util/formatter/LocalDateFormatter.java
@@ -0,0 +1,21 @@
+package com.genius.todoffin.util.formatter;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import org.springframework.format.Formatter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LocalDateFormatter implements Formatter<LocalDate> {
+
+    @Override
+    public LocalDate parse(String text, Locale locale) {
+        return LocalDate.parse(text, DateTimeFormatter.ISO_LOCAL_DATE);
+    }
+
+    @Override
+    public String print(LocalDate object, Locale locale) {
+        return DateTimeFormatter.ISO_LOCAL_DATE.format(object);
+    }
+}

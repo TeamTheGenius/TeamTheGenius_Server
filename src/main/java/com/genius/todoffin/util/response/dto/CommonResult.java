af5971f3cd09e23180c41eaeeaa9a8af3c48350e
SSung023 <50323157+SSung023@users.noreply.github.com>
2023-12-31 14:09:03 +0900

    feat: swagger 설정 파일 추가

--- src/main/java/com/genius/todoffin/util/response/dto/CommonResult.java
+++ src/main/java/com/genius/todoffin/util/response/dto/CommonResult.java
@@ -2,12 +2,10 @@ package com.genius.todoffin.util.response.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 
 @Getter
 @AllArgsConstructor
-@RequiredArgsConstructor
 public enum CommonResult {
     SUCCESS(1, "성공"),
     FAIL(0, "실패");

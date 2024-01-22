5b1c149da4e82c4c81e873199a10611d23a13030
SSung023 <50323157+SSung023@users.noreply.github.com>
2024-01-22 09:57:47 +0900

    feat: 테스트에 이용할 JWT 토큰 생성 유틸 클래스 구현

--- src/test/java/com/genius/gitget/security/controller/AuthControllerTest.java
+++ src/test/java/com/genius/gitget/security/controller/AuthControllerTest.java
@@ -1,14 +1,11 @@
 package com.genius.gitget.security.controller;
 
-import static com.genius.gitget.security.constants.JwtRule.ACCESS_PREFIX;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.genius.gitget.security.domain.UserPrincipal;
-import com.genius.gitget.security.service.JwtService;
 import com.genius.gitget.user.domain.Role;
-import com.genius.gitget.user.domain.User;
+import com.genius.gitget.util.TokenTestUtil;
 import com.genius.gitget.util.WithMockCustomUser;
 import jakarta.servlet.http.Cookie;
 import lombok.extern.slf4j.Slf4j;
@@ -17,8 +14,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
@@ -33,7 +28,7 @@ public class AuthControllerTest {
     WebApplicationContext context;
 
     @Autowired
-    JwtService jwtService;
+    TokenTestUtil tokenTestUtil;
 
     @BeforeEach
     public void setup() {
@@ -48,14 +43,7 @@ public class AuthControllerTest {
     @WithMockCustomUser(role = Role.USER)
     public void test() throws Exception {
         //given
-        UserPrincipal userPrincipal = (UserPrincipal) SecurityContextHolder.getContext().getAuthentication()
-                .getPrincipal();
-        User user = userPrincipal.getUser();
-
-        MockHttpServletResponse httpServletResponse = new MockHttpServletResponse();
-
-        String accessCookie = jwtService.generateAccessToken(httpServletResponse, user);
-        Cookie cookie = new Cookie(ACCESS_PREFIX.getValue(), accessCookie);
+        Cookie cookie = tokenTestUtil.createAccessCookie();
 
         //when&then
         mockMvc.perform(get("/api/test")

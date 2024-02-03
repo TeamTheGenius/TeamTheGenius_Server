86ba20dd8178640597337c17bc9f35ddadd8b8ac
SSung023 <50323157+SSung023@users.noreply.github.com>
2024-02-03 21:07:57 +0900

    test: 인증 컨트롤러 테스트 코드 추가

--- src/test/java/com/genius/gitget/util/WithMockCustomUserSecurityContextFactory.java
+++ src/test/java/com/genius/gitget/util/WithMockCustomUserSecurityContextFactory.java
@@ -1,14 +1,16 @@
 package com.genius.gitget.util;
 
-import com.genius.gitget.global.security.service.CustomUserDetailsService;
 import com.genius.gitget.challenge.user.domain.Role;
 import com.genius.gitget.challenge.user.domain.User;
 import com.genius.gitget.challenge.user.dto.SignupRequest;
 import com.genius.gitget.challenge.user.repository.UserRepository;
 import com.genius.gitget.challenge.user.service.UserService;
+import com.genius.gitget.global.security.service.CustomUserDetailsService;
 import java.util.List;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
@@ -23,16 +25,24 @@ public class WithMockCustomUserSecurityContextFactory implements WithSecurityCon
     private final UserService userService;
     private final CustomUserDetailsService customUserDetailsService;
 
+    @Value("${github.githubId}")
+    private String githubId;
+
     @Override
     public SecurityContext createSecurityContext(WithMockCustomUser customUser) {
+        String identifier = githubId;
+        if (!Objects.equals(customUser.identifier(), "identifier")) {
+            identifier = customUser.identifier();
+        }
+
         User user = User.builder()
                 .providerInfo(customUser.providerInfo())
-                .identifier(customUser.identifier())
+                .identifier(identifier)
                 .role(Role.NOT_REGISTERED)
                 .build();
 
         SignupRequest signupRequest = SignupRequest.builder()
-                .identifier(customUser.identifier())
+                .identifier(identifier)
                 .interest(List.of("FE", "BE"))
                 .nickname(customUser.nickname())
                 .information(customUser.information())

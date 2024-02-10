4acfc39c323a9835fa389187e2e8b873db74db4f
SSung023 <50323157+SSung023@users.noreply.github.com>
2024-02-10 12:06:32 +0900

    refactor: 암호화 클래스에 대해 상속 불가로 변경

--- src/main/java/com/genius/gitget/challenge/certification/util/EncryptUtil.java
+++ src/main/java/com/genius/gitget/challenge/certification/util/EncryptUtil.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class EncryptUtil {
+public final class EncryptUtil {
     private final AesBytesEncryptor encryptor;
 
     public String encryptPersonalToken(String personalToken) {

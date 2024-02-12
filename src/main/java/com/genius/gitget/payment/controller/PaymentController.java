6cd7dcf0329fa5f516fd5cf7892fbf4a104d665e
kimdozzi <neo5188@gmail.com>
2024-02-12 15:26:37 +0900

    chore: iamport dependency 추가

---
+++ src/main/java/com/genius/gitget/payment/controller/PaymentController.java
@@ -0,0 +1,32 @@
+package com.genius.gitget.payment.controller;
+
+import com.siot.IamportRestClient.IamportClient;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class PaymentController {
+
+    private IamportClient iamportClient;
+
+    @Value("${imp.api.key}")
+    private String apiKey;
+
+    @Value("${imp.api.secretkey}")
+    private String secretKey;
+
+    @PostConstruct
+    public void init() {
+        this.iamportClient = new IamportClient(apiKey, secretKey);
+    }
+
+
+}
---
+++ src/test/java/com/genius/gitget/payment/IamportRestTest.java
@@ -0,0 +1,23 @@
+package com.genius.gitget.payment;
+
+import com.siot.IamportRestClient.IamportClient;
+import com.siot.IamportRestClient.exception.IamportResponseException;
+import com.siot.IamportRestClient.response.AccessToken;
+import com.siot.IamportRestClient.response.IamportResponse;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+@Transactional
+public class IamportRestTest {
+
+}

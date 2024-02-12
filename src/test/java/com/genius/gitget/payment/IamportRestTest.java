e5418de60708f437ff1e01db7685f03aaed43261
kimdozzi <neo5188@gmail.com>
2024-02-12 15:27:01 +0900

    test: 사용자 access_token 정보 취득 테스트

--- src/test/java/com/genius/gitget/payment/IamportRestTest.java
+++ src/test/java/com/genius/gitget/payment/IamportRestTest.java
@@ -19,5 +19,36 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @Transactional
 public class IamportRestTest {
+    IamportClient client;
 
+    @Value("${imp.api.key}")
+    String apiKey;
+    @Value("${imp.api.secretkey}")
+    String secretKey;
+
+    @BeforeEach
+    public void setup() {
+        client = new IamportClient(apiKey, secretKey);
+    }
+
+    @Test
+    public void testGetToken() {
+        try {
+            IamportResponse<AccessToken> auth_response = client.getAuth();
+            System.out.println("auth_response.getToken() = " + auth_response.getResponse().getToken());
+            assertNotNull(auth_response.getResponse());
+            assertNotNull(auth_response.getResponse().getToken());
+        } catch (IamportResponseException e) {
+            System.out.println(e.getMessage());
+
+            switch (e.getHttpStatusCode()) {
+                case 401 :
+                    break;
+                case 500 :
+                    break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }

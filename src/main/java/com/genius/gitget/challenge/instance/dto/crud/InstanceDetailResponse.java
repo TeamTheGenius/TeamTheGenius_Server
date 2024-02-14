40159008d3b81bfa8f9d855fd2caac87dcd79995
HEY <50323157+SSung023@users.noreply.github.com>
2024-02-14 10:04:10 +0900

    [BUG] Topic 삭제 요청 시, 삭제가 되지 않는 버그 (#67)
    * fix: topic 삭제 시 예외가 발생하던 버그 픽스
    
    - topic 삭제 가능 조건에서 삭제 요청을 했을 때, 삭제가 되지 않고 예외가 발생하던 버그 픽스
    - 영속성 전이(cascade) 추가로 인해 발생하던 문제임을 인지 후 수정
    
    * refactor: File 로직 예외 추가 처리
    
    - File 시스템과 관련된 로직에서 따로 처리하지 않았던 부분에 대해 try-catch문을 통해 명시적으로 처리
    
    * feat: instance 생성 로직 보강
    
    - Topic에 등록된 이미지를 사용하기 위해, instance 생성 시 이미지를 별도로 전달하지 않은 경우에 대해 처리 로직 추가
    - FileUtil에 복사 관련 로직 추가 구현

--- src/main/java/com/genius/gitget/challenge/instance/dto/crud/InstanceDetailResponse.java
+++ src/main/java/com/genius/gitget/challenge/instance/dto/crud/InstanceDetailResponse.java
@@ -3,16 +3,16 @@ package com.genius.gitget.challenge.instance.dto.crud;
 import com.genius.gitget.challenge.instance.domain.Instance;
 import com.genius.gitget.global.file.domain.Files;
 import com.genius.gitget.global.file.dto.FileResponse;
-import lombok.Builder;
-
-import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Optional;
+import lombok.Builder;
 
 @Builder
-public record InstanceDetailResponse(Long topicId, Long instanceId, String title, String description, int pointPerPerson,
-                                     String tags, String notice, LocalDateTime startedAt, LocalDateTime completedAt, FileResponse fileResponse) {
-    public static InstanceDetailResponse createByEntity(Instance instance, Optional<Files> files) throws IOException {
+public record InstanceDetailResponse(Long topicId, Long instanceId, String title, String description,
+                                     int pointPerPerson,
+                                     String tags, String notice, LocalDateTime startedAt, LocalDateTime completedAt,
+                                     FileResponse fileResponse) {
+    public static InstanceDetailResponse createByEntity(Instance instance, Optional<Files> files) {
         return InstanceDetailResponse.builder()
                 .topicId(instance.getTopic().getId())
                 .instanceId(instance.getId())
@@ -27,7 +27,7 @@ public record InstanceDetailResponse(Long topicId, Long instanceId, String title
                 .build();
     }
 
-    private static FileResponse convertToFileResponse(Optional<Files> files) throws IOException {
+    private static FileResponse convertToFileResponse(Optional<Files> files) {
         if (files.isEmpty()) {
             return FileResponse.createNotExistFile();
         }
--- src/main/java/com/genius/gitget/global/file/service/FileUtil.java
+++ src/main/java/com/genius/gitget/global/file/service/FileUtil.java
@@ -1,6 +1,8 @@
 package com.genius.gitget.global.file.service;
 
+import static com.genius.gitget.global.util.exception.ErrorCode.FILE_NOT_COPIED;
 import static com.genius.gitget.global.util.exception.ErrorCode.FILE_NOT_EXIST;
+import static com.genius.gitget.global.util.exception.ErrorCode.IMAGE_NOT_ENCODED;
 import static com.genius.gitget.global.util.exception.ErrorCode.NOT_SUPPORTED_EXTENSION;
 
 import com.genius.gitget.global.file.domain.FileType;
@@ -8,8 +10,10 @@ import com.genius.gitget.global.file.domain.Files;
 import com.genius.gitget.global.file.dto.UpdateDTO;
 import com.genius.gitget.global.file.dto.UploadDTO;
 import com.genius.gitget.global.util.exception.BusinessException;
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.StandardCopyOption;
 import java.util.Base64;
 import java.util.List;
 import java.util.Objects;
@@ -20,11 +24,15 @@ import org.springframework.web.multipart.MultipartFile;
 public class FileUtil {
     private static final List<String> validExtensions = List.of("jpg", "jpeg", "png", "gif");
 
-    public static String encodedImage(Files files) throws IOException {
-        UrlResource urlResource = new UrlResource("file:" + files.getFileURI());
+    public static String encodedImage(Files files) {
+        try {
+            UrlResource urlResource = new UrlResource("file:" + files.getFileURI());
 
-        byte[] encode = Base64.getEncoder().encode(urlResource.getContentAsByteArray());
-        return new String(encode, StandardCharsets.UTF_8);
+            byte[] encode = Base64.getEncoder().encode(urlResource.getContentAsByteArray());
+            return new String(encode, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new BusinessException(IMAGE_NOT_ENCODED);
+        }
     }
 
     public static UploadDTO getUploadInfo(MultipartFile file, String typeStr, final String UPLOAD_PATH) {
@@ -49,6 +57,30 @@ public class FileUtil {
                 .savedFilename(savedFilename)
                 .fileURI(UPLOAD_PATH + fileType.getPath() + savedFilename)
                 .build();
+    }
+
+    public static UploadDTO getCopyInfo(Files files, FileType fileType, final String UPLOAD_PATH) {
+        String originalFilename = files.getOriginalFilename();
+        String savedFilename = getSavedFilename(originalFilename);
+
+        return UploadDTO.builder()
+                .fileType(fileType)
+                .originalFilename(originalFilename)
+                .savedFilename(savedFilename)
+                .fileURI(UPLOAD_PATH + fileType.getPath() + savedFilename)
+                .build();
+    }
+
+    public static void copyImage(String originFilePath, String copyFilePath) {
+        File originFile = new File(originFilePath);
+        File copyFile = new File(copyFilePath);
+
+        try {
+            java.nio.file.Files.copy(originFile.toPath(), copyFile.toPath(),
+                    StandardCopyOption.COPY_ATTRIBUTES);
+        } catch (IOException e) {
+            throw new BusinessException(FILE_NOT_COPIED);
+        }
     }
 
     public static void validateFile(MultipartFile file) {

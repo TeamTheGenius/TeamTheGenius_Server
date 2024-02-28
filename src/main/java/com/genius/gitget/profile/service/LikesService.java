4d77a91386fe74f239811b65e0c285a21e8ab436
kimdozzi <neo5188@gmail.com>
2024-02-28 22:54:02 +0900

    feat: mypage - 유저정보 조회/수정, 관심사 수정, 챌린지 현황 service 개발

---
+++ src/main/java/com/genius/gitget/profile/service/LikesService.java
@@ -0,0 +1,4 @@
+package com.genius.gitget.profile.service;
+
+public class LikesService {
+}
---
+++ src/main/java/com/genius/gitget/profile/service/ProfileService.java
@@ -0,0 +1,127 @@
+package com.genius.gitget.profile.service;
+
+import static com.genius.gitget.challenge.participantinfo.domain.JoinResult.FAIL;
+import static com.genius.gitget.challenge.participantinfo.domain.JoinResult.PROCESSING;
+import static com.genius.gitget.challenge.participantinfo.domain.JoinResult.SUCCESS;
+import static com.genius.gitget.challenge.participantinfo.domain.JoinStatus.YES;
+
+import com.genius.gitget.challenge.participantinfo.domain.JoinResult;
+import com.genius.gitget.challenge.participantinfo.domain.ParticipantInfo;
+import com.genius.gitget.challenge.user.domain.User;
+import com.genius.gitget.challenge.user.repository.UserRepository;
+import com.genius.gitget.global.file.domain.Files;
+import com.genius.gitget.global.file.repository.FilesRepository;
+import com.genius.gitget.global.file.service.FilesService;
+import com.genius.gitget.global.util.exception.BusinessException;
+import com.genius.gitget.global.util.exception.ErrorCode;
+import com.genius.gitget.profile.dto.UserChallengeResultResponse;
+import com.genius.gitget.profile.dto.UserInformationResponse;
+import com.genius.gitget.profile.dto.UserInformationUpdateRequest;
+import com.genius.gitget.profile.dto.UserTagsUpdateRequest;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+@Transactional(readOnly = true)
+public class ProfileService {
+    private final UserRepository userRepository;
+    private final FilesRepository filesRepository;
+    private final FilesService filesService;
+
+    // 마이페이지 - 사용자 정보 조회
+    public UserInformationResponse getUserInformation(String identifier) {
+        User findUser = findUser(identifier);
+        Files findFile = filesRepository.findById(findUser.getId())
+                .orElseThrow(() -> new BusinessException(ErrorCode.FILE_NOT_EXIST));
+
+        return UserInformationResponse.entityToDto(findUser, findFile);
+    }
+
+    // 마이페이지 - 사용자 정보 수정
+    @Transactional
+    public void updateUserInformation(UserInformationUpdateRequest userInformationUpdateRequest,
+                                      MultipartFile multipartFile) {
+        User findUser = findUser(userInformationUpdateRequest.getIdentifier());
+        findUser.updateUserInformation(
+                userInformationUpdateRequest.getNickname(),
+                userInformationUpdateRequest.getInformation());
+
+        Long findFileId = findUser.getFiles().getId();
+        filesService.updateFile(findFileId, multipartFile);
+        userRepository.save(findUser);
+    }
+
+    // 마이페이지 - 관심사 수정
+    @Transactional
+    public void updateUserTags(UserTagsUpdateRequest userTagsUpdateRequest) {
+        User findUser = findUser(userTagsUpdateRequest.getIdentifier());
+        findUser.updateUserTags(userTagsUpdateRequest.getTags());
+        userRepository.save(findUser);
+    }
+
+    // 마이페이지 - 챌린지 현황
+    public UserChallengeResultResponse getUserChallengeResult(String identifier) {
+        User findUser = findUser(identifier);
+        HashMap<JoinResult, List<Long>> participantHashMap = new HashMap<>() {
+            {
+                put(FAIL, new ArrayList<>());
+                put(PROCESSING, new ArrayList<>());
+                put(SUCCESS, new ArrayList<>());
+            }
+        };
+        // hashmap에 JoinResult : [1,2,3] 형식으로 저장할 예정 -> 1,2,3은 유저가 참여한 participantInfo.
+        // 다시 말해서, key는 JoinResult가 되고, values 에는 유저가 참여한 참여 정보의 PK인 id가 저장될 것 이다.
+
+        // 유저의 참여 정보를 담고 있는 리스트
+        List<ParticipantInfo> participantInfoList = findUser.getParticipantInfoList();
+        int participanTotalCount = participantInfoList.size();
+
+        for (int i = 0; i < participantInfoList.size(); i++) {
+            // 각 참여 정보를 받아옴.
+            if (participantInfoList.get(i).getJoinStatus() == YES) {
+                JoinResult joinResult = participantInfoList.get(i).getJoinResult();
+
+                // hashmap에 저장된 key와 일치 여부 확인
+                if (participantHashMap.containsKey(joinResult)) {
+                    // 일치한다면, 해당 key의 value인 list에 id 저장
+                    List<Long> participantIdList = participantHashMap.get(joinResult);
+                    participantIdList.add(participantInfoList.get(i).getId());
+
+                    // 최종적으로 hashmap에 저장
+                    participantHashMap.put(joinResult, participantIdList);
+                }
+            }
+        }
+
+        int fail = participantHashMap.get(FAIL).size();
+        int success = participantHashMap.get(SUCCESS).size();
+        int processing = participantHashMap.get(PROCESSING).size();
+
+        return UserChallengeResultResponse.builder()
+                .fail(fail)
+                .success(success)
+                .processing(processing)
+                .beforeStart(participanTotalCount - fail - success - processing)
+                .build();
+    }
+
+    public void deleteUserInformation(String identifier) {
+        User findUser = findUser(identifier);
+        // TODO : 회원 정보 삭제 시 고려해야 할 점 조사
+    }
+
+    private User findUser(String identifier) {
+        return userRepository.findByIdentifier(identifier)
+                .orElseThrow(() -> new BusinessException(ErrorCode.MEMBER_NOT_FOUND));
+    }
+
+
+}

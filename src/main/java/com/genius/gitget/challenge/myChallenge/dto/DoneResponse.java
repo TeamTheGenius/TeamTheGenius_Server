580906bd499b4480ad36d4a863b3e31a5dff0927
SSung023 <50323157+SSung023@users.noreply.github.com>
2024-03-05 21:21:38 +0900

    refactor: 완료 챌린지 응답 데이터 변경
    - 완료 챌린지의 응답 데이터에 '제목'과 '파일' 정보 추가
    - 관련 테스트 코드 추가

--- src/main/java/com/genius/gitget/challenge/myChallenge/dto/DoneResponse.java
+++ src/main/java/com/genius/gitget/challenge/myChallenge/dto/DoneResponse.java
@@ -4,38 +4,46 @@ import com.genius.gitget.challenge.instance.domain.Instance;
 import com.genius.gitget.challenge.participant.domain.JoinResult;
 import com.genius.gitget.challenge.participant.domain.Participant;
 import com.genius.gitget.challenge.participant.domain.RewardStatus;
+import com.genius.gitget.global.file.dto.FileResponse;
 import lombok.Builder;
 
 @Builder
 public record DoneResponse(
         Long instanceId,
+        String title,
         int pointPerPerson,
         JoinResult joinResult,
         boolean canGetReward,
         int numOfPointItem,
         int rewardedPoints,
-        double achievementRate
+        double achievementRate,
+        FileResponse fileResponse
 ) {
-    public static DoneResponse createNotRewarded(Instance instance, Participant participant,
+    public static DoneResponse createNotRewarded(Instance instance,
+                                                 Participant participant,
                                                  int numOfPointItem) {
         return DoneResponse.builder()
+                .title(instance.getTitle())
                 .instanceId(instance.getId())
                 .pointPerPerson(instance.getPointPerPerson())
                 .joinResult(participant.getJoinResult())
                 .canGetReward(canGetReward(participant))
                 .numOfPointItem(numOfPointItem)
+                .fileResponse(FileResponse.create(instance.getFiles()))
                 .build();
     }
 
     public static DoneResponse createRewarded(Instance instance, Participant participant,
                                               double achievementRate) {
         return DoneResponse.builder()
+                .title(instance.getTitle())
                 .instanceId(instance.getId())
                 .pointPerPerson(instance.getPointPerPerson())
                 .joinResult(participant.getJoinResult())
                 .canGetReward(false)
                 .rewardedPoints(participant.getRewardPoints())
                 .achievementRate(achievementRate)
+                .fileResponse(FileResponse.create(instance.getFiles()))
                 .build();
     }
 

094febf3c7b5631f492c51da6850c5ade69c320d
SSung023 <50323157+SSung023@users.noreply.github.com>
2024-03-11 11:56:00 +0900

    fix: 서버 실행 시, 아이템 정보가 추가되도록 설정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,4 @@
-# 단독 실행 시 정상 작동하지만, ddl-auto를 create로 해놓고 실행하면 오류 발생
-INSERT INTO todoffin.item (cost, created_at, deleted_at, updated_at, details, name, item_category)
+INSERT INTO item (cost, created_at, deleted_at, updated_at, details, name, item_category)
 VALUES (100, NULL, NULL, NULL, '프로필 프레임', 'profile frame', 'PROFILE_FRAME'),
        (100, NULL, NULL, NULL, '인증 패스 아이템', 'certification passer', 'CERTIFICATION_PASSER'),
        (100, NULL, NULL, NULL, '포인트 2배 획득 아이템', 'point multiplier', 'POINT_MULTIPLIER');

fe680d3392ab791235983fc0c63fc8e8ea655a74
SSung023 <50323157+SSung023@users.noreply.github.com>
2024-03-11 15:09:59 +0900

    fix: Items에 이미 값이 존재하는 경우 데이터를 삽입하지 않도록 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,14 @@
 INSERT INTO item (cost, created_at, deleted_at, updated_at, details, name, item_category)
-VALUES (100, NULL, NULL, NULL, '프로필 프레임', 'profile frame', 'PROFILE_FRAME'),
-       (100, NULL, NULL, NULL, '인증 패스 아이템', 'certification passer', 'CERTIFICATION_PASSER'),
-       (100, NULL, NULL, NULL, '포인트 2배 획득 아이템', 'point multiplier', 'POINT_MULTIPLIER');
+SELECT *
+FROM (SELECT 100             AS cost,
+             NULL            AS created_at,
+             NULL            AS deleted_at,
+             NULL            AS updated_at,
+             '프로필 프레임'       AS details,
+             'profile frame' AS name,
+             'PROFILE_FRAME' AS item_category
+      UNION ALL
+      SELECT 100, NULL, NULL, NULL, '인증 패스 아이템', 'certification passer', 'CERTIFICATION_PASSER'
+      UNION ALL
+      SELECT 100, NULL, NULL, NULL, '포인트 2배 획득 아이템', 'point multiplier', 'POINT_MULTIPLIER') AS new_items
+WHERE (SELECT COUNT(*) FROM item) < 3;

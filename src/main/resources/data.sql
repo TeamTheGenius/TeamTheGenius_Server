57392b3b6d53b233ce9fd781c95f1dd83fd54ca5
kimdozzi <neo5188@gmail.com>
2024-03-11 18:29:27 +0900

    hotfix: type definition error 해결

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO item (cost, created_at, deleted_at, updated_at, details, name, item_category)
-VALUES (100, NULL, NULL, NULL, '프로필 프레임', 'profile frame', 'PROFILE_FRAME'),
-       (100, NULL, NULL, NULL, '인증 패스 아이템', 'certification passer', 'CERTIFICATION_PASSER'),
-       (100, NULL, NULL, NULL, '포인트 2배 획득 아이템', 'point multiplier', 'POINT_MULTIPLIER');

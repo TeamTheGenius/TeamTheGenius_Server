bd33d5eafe8daa7a3a7f2be7b6c2f8f83f1ea377
SSung023 <50323157+SSung023@users.noreply.github.com>
2024-03-19 14:48:08 +0900

    feat: 사용자 정보 조회 API 응답 데이터에 프레임 정보 추가

--- src/main/java/com/genius/gitget/profile/dto/UserInformationResponse.java
+++ src/main/java/com/genius/gitget/profile/dto/UserInformationResponse.java
@@ -11,19 +11,22 @@ import lombok.Data;
 public class UserInformationResponse {
     private String identifier;
     private String nickname;
+    private Long frameId;
     private FileResponse fileResponse;
 
     @Builder
-    public UserInformationResponse(String identifier, String nickname, Files files) {
+    public UserInformationResponse(String identifier, String nickname, Long frameId, Files files) {
         this.identifier = identifier;
         this.nickname = nickname;
+        this.frameId = frameId;
         this.fileResponse = convertToFileResponse(Optional.ofNullable(files));
     }
 
-    public static UserInformationResponse createByEntity(User findUser, Files files) {
+    public static UserInformationResponse createByEntity(User findUser, Long frameId, Files files) {
         return UserInformationResponse.builder()
                 .identifier(findUser.getIdentifier())
                 .nickname(findUser.getNickname())
+                .frameId(frameId)
                 .files(files)
                 .build();
     }

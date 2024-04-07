c9cb89f9f50aaf214b26ada96d5aeab6c83c77be
SSung023 <50323157+SSung023@users.noreply.github.com>
2024-04-07 21:44:04 +0900

    chore: ec2 배포 자동화 테스트

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,19 +1,14 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-PROJECT_ROOT="/home/ubuntu/app"
-JAR_FILE="$PROJECT_ROOT/GitGetApplication.jar"
+ROOT_PATH="/home/ubuntu/build"
+JAR="$ROOT_PATH/GitGetApplication.jar"
+STOP_LOG="$ROOT_PATH/stop.log"
+SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
 
-DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
-
-TIME_NOW=$(date +%c)
-
-# 현재 구동 중인 애플리케이션 pid 확인
-CURRENT_PID=$(pgrep -f $JAR_FILE)
-
-# 프로세스가 켜져 있으면 종료
-if [ -z "$CURRENT_PID" ]; then
-  echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
+if [ -z "$SERVICE_PID" ]; then
+  echo "서비스 NotFound" >> $STOP_LOG
 else
-  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
-  kill -15 "$CURRENT_PID"
+  echo "서비스 종료 " >> $STOP_LOG
+#  kill "$SERVICE_PID"
+   kill -9 "$SERVICE_PID" # 강제 종료를 하고 싶다면 이 명령어 사용
 fi

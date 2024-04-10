4af7f9abb67ec2b05f29bd39e7c0aee88850a070
SSung023 <50323157+SSung023@users.noreply.github.com>
2024-04-10 12:11:15 +0900

    chore: deploy.sh 버그 픽스

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ JAR_NAME=$(basename $BUILD_JAR)
 echo ">>> build 파일명: $JAR_NAME" >> /home/ubuntu/deploy.log
 
 echo ">>> build 파일 복사" >> /home/ubuntu/deploy.log
-DEPLOY_PATH=/home/ubuntu/app
+DEPLOY_PATH=/home/ubuntu/app/
 cp $BUILD_JAR $DEPLOY_PATH
 
 echo ">>> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/deploy.log
@@ -22,6 +22,7 @@ fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo ">>> DEPLOY_JAR 배포"    >> /home/ubuntu/deploy.log
+echo ">>> $DEPLOY_JAR의 $JAR_NAME를 실행합니다"
 nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2> /home/ubuntu/deploy_err.log &
 
 # backup

bd355abef03d5e52c3b08b603aea01dce67ced11
SSung023 <50323157+SSung023@users.noreply.github.com>
2024-04-11 16:07:05 +0900

    chore: java 어플리케이션 삭제하도록 설정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,16 +9,17 @@ DEPLOY_PATH=/home/ubuntu/app/
 cp $BUILD_JAR $DEPLOY_PATH
 
 echo ">>> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/deploy.log
-CURRENT_PID=$(pgrep -f $JAR_NAME)
-
-if [ -z $CURRENT_PID ]
-then
-  echo ">>> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/deploy.log
-else
-  echo ">>> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
-  sleep 5
-fi
+pkill -9 -ef java
+#CURRENT_PID=$(pgrep -f $JAR_NAME)
+#
+#if [ -z $CURRENT_PID ]
+#then
+#  echo ">>> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/deploy.log
+#else
+#  echo ">>> kill -15 $CURRENT_PID"
+#  kill -15 $CURRENT_PID
+#  sleep 5
+#fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo ">>> DEPLOY_JAR 배포"    >> /home/ubuntu/deploy.log

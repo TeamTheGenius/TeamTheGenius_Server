e4398460f465028c9237a861e6b0851c9e567a75
SSung023 <50323157+SSung023@users.noreply.github.com>
2024-04-11 20:51:30 +0900

    chore: 배포 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,17 +10,6 @@ cp $BUILD_JAR $DEPLOY_PATH
 
 echo ">>> 현재 실행중인 애플리케이션 pid 확인 후 일괄 종료" >> /home/ubuntu/deploy.log
 sudo ps -ef | grep java | awk '{print $2}' | xargs kill -15
-#pkill -9 -ef java
-#CURRENT_PID=$(pgrep -f $JAR_NAME)
-#
-#if [ -z $CURRENT_PID ]
-#then
-#  echo ">>> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/deploy.log
-#else
-#  echo ">>> kill -15 $CURRENT_PID"
-#  kill -15 $CURRENT_PID
-#  sleep 5
-#fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo ">>> DEPLOY_JAR 배포"    >> /home/ubuntu/deploy.log

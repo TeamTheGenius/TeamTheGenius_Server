b8fa620c01ea4f2215c19732e02cb426f0965d91
kimdozzi <neo5188@gmail.com>
2024-04-17 16:50:47 +0900

    test: 무중단 배포 테스트

--- scripts/health.sh
+++ scripts/health.sh
@@ -1,40 +1,35 @@
-#!/usr/bin/env bash
+# health_check.sh
 
-# Nginx와 연결되지 않은 포트로 스프링 부트가 잘 수행되었는지 체크
-ABSPATH=$(readlink -f $0)
-ABSDIR=$(dirname $ABSPATH)
-source ${ABSDIR}/profile.sh
-source ${ABSDIR}/switch.sh
+#!/bin/bash
 
-IDLE_PORT=$(find_idle_port)
+# Crawl current connected port of WAS
+CURRENT_PORT=$(cat /home/ec2-user/service_url.inc | grep -Po '[0-9]+' | tail -1)
+TARGET_PORT=0
 
-echo "> Health Check Start!"
-echo "> IDLE_PORT: $IDLE_PORT"
-echo "> curl -s http://localhost:$IDLE_PORT/profile "
-sleep 10
-
-for RETRY_COUNT in {1..10}
-do
-  RESPONSE=$(curl -s http://localhost:${IDLE_PORT}/profile)
-  UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -l)
-
-  if [ ${UP_COUNT} -ge 1 ]
-  then # $up_count >= 1 ("real" 문자열이 있는지 검증)
-      echo "> Health check 성공"
-      switch_proxy # 잘 떳다면, switch.sh의 switch_proxy로 Nginx 프록시 설정을 변경
-      break
-  else
-      echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
-      echo "> Health check: ${RESPONSE}"
-  fi
-
-  if [ ${RETRY_COUNT} -eq 10 ]
-  then
-    echo "> Health check 실패. "
-    echo "> 엔진엑스에 연결하지 않고 배포를 종료합니다."
+# Toggle port Number
+if [ ${CURRENT_PORT} -eq 8080 ]; then
+    TARGET_PORT=8081
+elif [ ${CURRENT_PORT} -eq 8081 ]; then
+    TARGET_PORT=8080
+else
+    echo "> No WAS is connected to nginx"
     exit 1
-  fi
+fi
 
-  echo "> Health check 연결 실패. 재시도..."
-  sleep 10
+
+echo "> Start health check of WAS at 'http://127.0.0.1:${TARGET_PORT}' ..."
+
+for RETRY_COUNT in 1 2 3 4 5 6 7 8 9 10
+do
+    echo "> #${RETRY_COUNT} trying..."
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}"  http://127.0.0.1:${TARGET_PORT}/health)
+
+    if [ ${RESPONSE_CODE} -eq 200 ]; then
+        echo "> New WAS successfully running"
+        exit 0
+    elif [ ${RETRY_COUNT} -eq 10 ]; then
+        echo "> Health check failed."
+        exit 1
+    fi
+    sleep 10
 done
--- scripts/switch.sh
+++ scripts/switch.sh
@@ -1,19 +1,29 @@
-#!/usr/bin/env bash
+# switch.sh
 
-ABSPATH=$(readlink -f $0)
-ABSDIR=$(dirname $ABSPATH)
-source ${ABSDIR}/profile.sh
+#!/bin/bash
 
-function switch_proxy() {
-    IDLE_PORT=$(find_idle_port)
+# Crawl current connected port of WAS
+CURRENT_PORT=$(cat /home/ec2-user/service_url.inc  | grep -Po '[0-9]+' | tail -1)
+TARGET_PORT=0
 
-    echo "> 전환할 Port: $IDLE_PORT"
-    echo "> Port 전환"
-    # 하나의 문장을 만들어 파이프라인(|)으로 넘겨 주기 위해 echo를 사용. tee 는 앞의 문장을 읽어 해당 경로의 파일에 저장.
-    echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
+echo "> Nginx currently proxies to ${CURRENT_PORT}."
 
-    echo "> 엔진엑스 Reload"
-    # restart와 달리 끊김 없이 다시 불러온다. (중요한 설정을 반영해야한다면 restart 사용)
-    # 여기선 외부의 설정 파일인 service-url을 다시 불러오기 때문에 reload로 가능
-    sudo service nginx reload
-}
+# Toggle port number
+if [ ${CURRENT_PORT} -eq 8080 ]; then
+    TARGET_PORT=8081
+elif [ ${CURRENT_PORT} -eq 8081 ]; then
+    TARGET_PORT=8080
+else
+    echo "> No WAS is connected to nginx"
+    exit 1
+fi
+
+# Change proxying port into target port
+echo "set \$service_url http://127.0.0.1:${TARGET_PORT};" | tee /home/ec2-user/service_url.inc
+
+echo "> Now Nginx proxies to ${TARGET_PORT}."
+
+# Reload nginx
+sudo service nginx reload
+
+echo "> Nginx reloaded."

901e6448fe1749d945af2a412383f33976f2b4fe
kimdozzi <neo5188@gmail.com>
2024-04-17 23:11:55 +0900

    test: 무중단 배포 테스트

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -9,10 +9,10 @@ TARGET_PORT=0
 echo "> Nginx currently proxies to ${CURRENT_PORT}."
 
 # Toggle port number
-if [ ${CURRENT_PORT} -eq 8080 ]; then
+if [ ${CURRENT_PORT} -eq 8081 ]; then
+    TARGET_PORT=8082
+elif [ ${CURRENT_PORT} -eq 8082 ]; then
     TARGET_PORT=8081
-elif [ ${CURRENT_PORT} -eq 8081 ]; then
-    TARGET_PORT=8080
 else
     echo "> No WAS is connected to nginx"
     exit 1

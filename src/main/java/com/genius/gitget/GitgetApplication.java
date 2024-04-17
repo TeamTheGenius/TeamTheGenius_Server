12dc46eca9499bc3e320f0e062f256cd97a22fcc
kimdozzi <neo5188@gmail.com>
2024-04-18 00:40:46 +0900

    test: 무중단 배포 테스트

--- src/main/java/com/genius/gitget/GitgetApplication.java
+++ src/main/java/com/genius/gitget/GitgetApplication.java
@@ -11,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing
 @EnableMongoRepositories
 public class GitgetApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(GitgetApplication.class, args);
     }

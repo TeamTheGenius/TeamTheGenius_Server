58fdefe968f4588b4cfbafae263248af0f6129cb
kimdozzi <neo5188@gmail.com>
2024-04-17 16:20:26 +0900

    feat: scripts 파일 작성 및 port switch를 위한 controller 생성
    - health, start, stop, profile, switch 스크립트 파일 생성
    - 포트 번호별 스프링 부트 switch을 위한 controller 생성

--- scripts/health.sh
+++ scripts/health.sh
@@ -21,7 +21,7 @@ do
   if [ ${UP_COUNT} -ge 1 ]
   then # $up_count >= 1 ("real" 문자열이 있는지 검증)
       echo "> Health check 성공"
-      switch_proxy # 잘 떴다면, switch.sh의 switch_proxy로 Nginx 프록시 설정을 변경
+      switch_proxy # 잘 떳다면, switch.sh의 switch_proxy로 Nginx 프록시 설정을 변경
       break
   else
       echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
@@ -31,7 +31,7 @@ do
   if [ ${RETRY_COUNT} -eq 10 ]
   then
     echo "> Health check 실패. "
-    echo "> Nginx에 연결하지 않고 배포를 종료합니다."
+    echo "> 엔진엑스에 연결하지 않고 배포를 종료합니다."
     exit 1
   fi
 
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,14 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-ROOT_PATH="/home/ubuntu/build"
-JAR="$ROOT_PATH/GitGetApplication.jar"
-STOP_LOG="$ROOT_PATH/stop.log"
-SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
+ABSPATH=$(readlink -f $0) # 절대경로
+ABSDIR=$(dirname $ABSPATH ) # 현재 stop.sh가 속해있는 경로를 찾는다.
+source ${ABSDIR}/profile.sh # 자바로 보면 일종의 import 구문, stop.sh에서도 profile.sh의 여러 function을 사용 가능
 
-if [ -z "$SERVICE_PID" ]; then
-  echo "서비스 NotFound" >> $STOP_LOG
+IDLE_PORT=$(find_idle_port)
+
+echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인"
+IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
+
+if [ -z ${IDLE_PID} ]
+then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "서비스 종료 " >> $STOP_LOG
-#  kill "$SERVICE_PID"
-   kill -9 "$SERVICE_PID" # 강제 종료를 하고 싶다면 이 명령어 사용
+  echo "> kill -15 $IDLE_PID"
+  kill -15 ${IDLE_PID}
+  sleep 5
 fi

713b1425f983bee66315be67c50de17879e2aad9
kimdozzi <neo5188@gmail.com>
2024-04-17 23:31:07 +0900

    test: 무중단 배포 테스트

--- scripts/health.sh
+++ scripts/health.sh
@@ -7,10 +7,10 @@ CURRENT_PORT=$(cat /home/ubuntu/service_url.inc | grep -Po '[0-9]+' | tail -1)
 TARGET_PORT=0
 
 # Toggle port Number
-if [ ${CURRENT_PORT} -eq 8080 ]; then
+if [ ${CURRENT_PORT} -eq 8081 ]; then
+  TARGET_PORT=8082
+elif [ ${CURRENT_PORT} -eq 8082 ]; then
   TARGET_PORT=8081
-elif [ ${CURRENT_PORT} -eq 8081 ]; then
-  TARGET_PORT=8080
 else
     echo "> No WAS is connected to nginx"
     exit 1

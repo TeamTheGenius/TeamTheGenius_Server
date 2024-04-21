8f265565d3dd3c1f37ed88f1b834fc7ec77ff6ae
kimdozzi <neo5188@gmail.com>
2024-04-21 22:58:29 +0900

    bug: 좋아요 목록 조회 오류 테스트 완료

--- src/main/java/com/genius/gitget/challenge/likes/service/LikesService.java
+++ src/main/java/com/genius/gitget/challenge/likes/service/LikesService.java
@@ -59,7 +59,6 @@ public class LikesService {
         }
         int start = (int) pageable.getOffset();
         int end = Math.min((start + pageable.getPageSize()), userLikesResponses.size());
-        log.info("페이징" + start + " " + end);
         return new PageImpl<>(userLikesResponses.subList(start, end), pageable, userLikesResponses.size());
     }
 

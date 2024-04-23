d01a16a6f3db2f9b6c5ca07f0d36185894205829
kimdozzi <neo5188@gmail.com>
2024-04-23 21:59:17 +0900

    feat: 무중단 배포 스크립트 파일

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,21 +1,21 @@
-##!/bin/bash
-#
-#BUILD_JAR=$(ls /home/ubuntu/app/build/libs/*.jar)
-#JAR_NAME=$(basename $BUILD_JAR)
-#echo ">>> build 파일명: $JAR_NAME" >> /home/ubuntu/deploy.log
-#
-#echo ">>> build 파일 복사" >> /home/ubuntu/deploy.log
-#DEPLOY_PATH=/home/ubuntu/app/
-#cp $BUILD_JAR $DEPLOY_PATH
-#
-#echo ">>> 현재 실행중인 애플리케이션 pid 확인 후 일괄 종료" >> /home/ubuntu/deploy.log
-#sudo ps -ef | grep java | awk '{print $2}' | xargs kill -15
-#
-#DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-#echo ">>> DEPLOY_JAR 배포"    >> /home/ubuntu/deploy.log
-#echo ">>> $DEPLOY_JAR의 $JAR_NAME를 실행합니다" >> /home/ubunru/deploy.log
-#nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2> /home/ubuntu/deploy_err.log &
-#
+#!/bin/bash
+
+BUILD_JAR=$(ls /home/ubuntu/app/build/libs/*.jar)
+JAR_NAME=$(basename $BUILD_JAR)
+echo ">>> build 파일명: $JAR_NAME" >> /home/ubuntu/deploy.log
+
+echo ">>> build 파일 복사" >> /home/ubuntu/deploy.log
+DEPLOY_PATH=/home/ubuntu/app/
+cp $BUILD_JAR $DEPLOY_PATH
+
+echo ">>> 현재 실행중인 애플리케이션 pid 확인 후 일괄 종료" >> /home/ubuntu/deploy.log
+sudo ps -ef | grep java | awk '{print $2}' | xargs kill -15
+
+DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+echo ">>> DEPLOY_JAR 배포"    >> /home/ubuntu/deploy.log
+echo ">>> $DEPLOY_JAR의 $JAR_NAME를 실행합니다" >> /home/ubunru/deploy.log
+nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2> /home/ubuntu/deploy_err.log &
+
 ## backup
 ##BUILD_JAR=$(ls /home/ubuntu/app/build/libs/*.jar)
 ##JAR_NAME=$(basename $BUILD_JAR)
--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -25,6 +25,6 @@ if [ ! -z ${TARGET_PID} ]; then
   sudo kill ${TARGET_PID}
 fi
 
-nohup java -jar -Dserver.port=${TARGET_PORT} /home/ubuntu/app/build/libs/* > /home/ubuntu/nohup.out 2>&1 &
+nohup java -jar -Dserver.port=${TARGET_PORT} /home/ubuntu/app/build/libs/*.jar > /home/ubuntu/nohup.out 2>&1 &
 echo "> Now new WAS runs at ${TARGET_PORT}."
 exit 0

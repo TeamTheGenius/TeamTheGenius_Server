8e647f07ca1e76ceca5f42b9916af49b4fde5b56
DoHyung Kim <neo5188@gmail.com>
2024-05-24 16:03:38 +0900

    [FEAT] AWS S3 파일 저장 이슈 해결 (#181)
    * feat: aws s3 bucket access issue
    
    - @CrossOrigin 어노테이션 추가
    
    * feat: aws 적용 테스트

--- src/main/java/com/genius/gitget/challenge/myChallenge/controller/MyChallengeController.java
+++ src/main/java/com/genius/gitget/challenge/myChallenge/controller/MyChallengeController.java
@@ -15,6 +15,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/challenges")
 @RequiredArgsConstructor
+@CrossOrigin
 public class MyChallengeController {
     private final MyChallengeService myChallengeService;
 

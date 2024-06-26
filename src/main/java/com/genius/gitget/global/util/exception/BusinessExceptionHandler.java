02635c22b144bc01655960f026d9cc1716c4fb0e
SSung023 <50323157+SSung023@users.noreply.github.com>
2024-06-26 16:48:16 +0900

    feat: BusinessException 발생 시, Slack 채널에 메세지 보내는 기능 추가
    - BusinessException 발생 시, Slack의 #exception-log 채널에 메세지를 보내는 기능 추가
    - GlobalException에 대한 추가 처리도 필요
    - .yml 파일에 slack 정보 추가  필요
    - 스택 트레이스 가독성 개선 필요

--- src/main/java/com/genius/gitget/global/util/exception/BusinessExceptionHandler.java
+++ src/main/java/com/genius/gitget/global/util/exception/BusinessExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.genius.gitget.global.util.exception;
 
 import com.genius.gitget.global.util.response.dto.CommonResponse;
+import com.genius.gitget.slack.service.SlackService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -11,9 +12,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 @RequiredArgsConstructor
 public class BusinessExceptionHandler {
+    private final SlackService slackService;
+
     @ExceptionHandler(BusinessException.class)
     protected ResponseEntity<CommonResponse> globalBusinessExceptionHandler(BusinessException e) {
         log.error("[ERROR]" + e.getMessage(), e);
+        slackService.sendMessage(e);
 
         return ResponseEntity.badRequest().body(
                 new CommonResponse(e.getStatus(), e.getMessage())

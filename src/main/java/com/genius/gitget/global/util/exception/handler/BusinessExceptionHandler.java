3cffcc0c958a4ebcbb72c5359e41f5f6ad7723c2
SSung023 <50323157+SSung023@users.noreply.github.com>
2024-06-30 21:28:53 +0900

    fix: prod 프로파일이 활성화되어 있을 때에만 메세지를 보내도록 설정
    - Environment를 의존성 주입받아 prod(application-prod.yml) 프로파일이 활성화되어 있을 때에만 Slack 메세지를 보내도록 설정
    - 각 ExceptionHandler가 MessageSender 인터페이스를 상속받고, sendSlackMessage() 메서드를 구현
    - 테스트 및 개발 환경에서 Slack 메세지를 보내지 않는 점 확인

--- src/main/java/com/genius/gitget/global/util/exception/handler/BusinessExceptionHandler.java
+++ src/main/java/com/genius/gitget/global/util/exception/handler/BusinessExceptionHandler.java
@@ -1,9 +1,11 @@
-package com.genius.gitget.global.util.exception;
+package com.genius.gitget.global.util.exception.handler;
 
+import com.genius.gitget.global.util.exception.BusinessException;
 import com.genius.gitget.global.util.response.dto.CommonResponse;
 import com.genius.gitget.slack.service.SlackService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,16 +13,26 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 @RestControllerAdvice
 @RequiredArgsConstructor
-public class BusinessExceptionHandler {
+public class BusinessExceptionHandler implements MessageSender {
+    private final Environment env;
     private final SlackService slackService;
 
     @ExceptionHandler(BusinessException.class)
     protected ResponseEntity<CommonResponse> globalBusinessExceptionHandler(BusinessException e) {
         log.error("[ERROR]" + e.getMessage(), e);
-        slackService.sendMessage(e);
+        sendSlackMessage(e);
 
         return ResponseEntity.badRequest().body(
                 new CommonResponse(e.getStatus(), e.getMessage())
         );
     }
+
+    @Override
+    public void sendSlackMessage(Exception exception) {
+        if (!env.matchesProfiles("prod")) {
+            return;
+        }
+        slackService.sendMessage(exception);
+    }
+
 }

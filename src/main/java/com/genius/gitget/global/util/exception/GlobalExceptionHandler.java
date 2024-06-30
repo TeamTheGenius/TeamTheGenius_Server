d6acf5463618110cc80295286346c9a3c988ca7f
SSung023 <50323157+SSung023@users.noreply.github.com>
2024-06-30 12:05:15 +0900

    feat: 처리하지 않은 예외 외에 다른 예외 발생 시에도 Slack 메세지 보내도록 설정

--- src/main/java/com/genius/gitget/global/util/exception/GlobalExceptionHandler.java
+++ src/main/java/com/genius/gitget/global/util/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.genius.gitget.global.util.exception;
 
 import com.genius.gitget.global.util.response.dto.CommonResponse;
+import com.genius.gitget.slack.service.SlackService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -12,11 +13,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 @RequiredArgsConstructor
 public class GlobalExceptionHandler {
+    private final SlackService slackService;
 
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<CommonResponse> globalExceptionHandler(Exception e) {
         log.error("예외처리 되지 않은 Exception 발생 - 처리 필요");
         log.error("[UNHANDLED ERROR] " + e.getMessage(), e);
+        slackService.sendMessage(e);
+
         return ResponseEntity.badRequest().body(
                 new CommonResponse(HttpStatus.BAD_REQUEST, e.getMessage()));
     }

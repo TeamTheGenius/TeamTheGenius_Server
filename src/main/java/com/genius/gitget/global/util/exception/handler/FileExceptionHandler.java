36c6ce595b92f00685ed82677845cecdce429ffc
HEY <50323157+SSung023@users.noreply.github.com>
2024-07-10 01:32:00 +0900

    [FEAT] 예외 발생 시, Slack 채널에 메세지를 보내는 기능 추가 (#214)
    * chore: slack 의존성 추가 및 불필요한 주석 제거
    
    * chore: favicon error 해결을 위해 favicon.ico 추가
    
    * feat: BusinessException 발생 시, Slack 채널에 메세지 보내는 기능 추가
    
    - BusinessException 발생 시, Slack의 #exception-log 채널에 메세지를 보내는 기능 추가
    - GlobalException에 대한 추가 처리도 필요
    - .yml 파일에 slack 정보 추가  필요
    - 스택 트레이스 가독성 개선 필요
    
    * feat: Exception 발생 시 보내는 메세지 포멧 변경
    
    - Exception 발생 시 전송하는 Slack 메세지에 예외 발생 시각 데이터 추가
    
    * feat: 처리하지 않은 예외 외에 다른 예외 발생 시에도 Slack 메세지 보내도록 설정
    
    * fix: prod 프로파일이 활성화되어 있을 때에만 메세지를 보내도록 설정
    
    - Environment를 의존성 주입받아 prod(application-prod.yml) 프로파일이 활성화되어 있을 때에만 Slack 메세지를 보내도록 설정
    - 각 ExceptionHandler가 MessageSender 인터페이스를 상속받고, sendSlackMessage() 메서드를 구현
    - 테스트 및 개발 환경에서 Slack 메세지를 보내지 않는 점 확인

--- src/main/java/com/genius/gitget/global/util/exception/handler/FileExceptionHandler.java
+++ src/main/java/com/genius/gitget/global/util/exception/handler/FileExceptionHandler.java
@@ -1,9 +1,12 @@
-package com.genius.gitget.global.util.exception;
+package com.genius.gitget.global.util.exception.handler;
 
 import static com.genius.gitget.global.util.exception.ErrorCode.FILE_MAX_SIZE_EXCEED;
 
 import com.genius.gitget.global.util.response.dto.CommonResponse;
+import com.genius.gitget.slack.service.SlackService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,11 +15,25 @@ import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 @Slf4j
 @RestControllerAdvice
-public class FileExceptionHandler {
+@RequiredArgsConstructor
+public class FileExceptionHandler implements MessageSender {
+    private final Environment env;
+    private final SlackService slackService;
+
     @ExceptionHandler(MaxUploadSizeExceededException.class)
     protected ResponseEntity<CommonResponse> globalExceptionHandler(Exception e) {
         log.error("Multipart 용량이 최대 크기를 초과하여 예외가 발생했습니다.");
+        sendSlackMessage(e);
+
         return ResponseEntity.badRequest().body(
                 new CommonResponse(HttpStatus.BAD_REQUEST, FILE_MAX_SIZE_EXCEED.getMessage()));
+    }
+
+    @Override
+    public void sendSlackMessage(Exception exception) {
+        if (!env.matchesProfiles("prod")) {
+            return;
+        }
+        slackService.sendMessage(exception);
     }
 }

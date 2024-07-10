48bc68d1dee76c9c2e94cb64a3c48e588befb38e
SSung023 <50323157+SSung023@users.noreply.github.com>
2024-07-10 17:39:14 +0900

    refactor: 필요없는 구문 삭제

--- src/main/java/com/genius/gitget/global/security/service/CustomUserDetailsService.java
+++ src/main/java/com/genius/gitget/global/security/service/CustomUserDetailsService.java
@@ -2,14 +2,13 @@ package com.genius.gitget.global.security.service;
 
 import static com.genius.gitget.global.util.exception.ErrorCode.MEMBER_NOT_FOUND;
 
-import com.genius.gitget.global.security.domain.UserPrincipal;
 import com.genius.gitget.challenge.user.domain.User;
 import com.genius.gitget.challenge.user.repository.UserRepository;
+import com.genius.gitget.global.security.domain.UserPrincipal;
 import com.genius.gitget.global.util.exception.BusinessException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,7 +17,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username) {
         User user = userRepository.findById(Long.valueOf(username))
                 .orElseThrow(() -> new BusinessException(MEMBER_NOT_FOUND));
 

d15ccee6070a697ccd5536bb5b3c94fe2d33b3c8
SSung023 <50323157+SSung023@users.noreply.github.com>
2024-08-03 23:27:18 +0900

    fix: 인증 조건을 검증하는 코드 누락 수정

--- src/main/java/com/genius/gitget/store/item/facade/StoreFacadeService.java
+++ src/main/java/com/genius/gitget/store/item/facade/StoreFacadeService.java
@@ -146,6 +146,8 @@ public class StoreFacadeService implements StoreFacade {
         User user = orders.getUser();
         Instance instance = instanceService.findInstanceById(instanceId);
         Participant participant = participantProvider.findByJoinInfo(user.getId(), instanceId);
+        
+        participant.validateRewardCondition();
 
         int rewardPoints = instance.getPointPerPerson() * 2;
         user.updatePoints((long) rewardPoints);

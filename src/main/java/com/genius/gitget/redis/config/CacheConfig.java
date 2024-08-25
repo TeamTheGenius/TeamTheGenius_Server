206721aa482509d8ed241f2fabc65ac01064aeab
kimdozzi <neo5188@gmail.com>
2024-08-25 10:48:56 +0900

    feat: redis cache 사용을 위한 config 파일

--- src/main/java/com/genius/gitget/redis/config/CacheConfig.java
+++ src/main/java/com/genius/gitget/redis/config/CacheConfig.java
@@ -1,2 +1,45 @@
-package com.genius.gitget.redis.config;public class CacheConfig {
+package com.genius.gitget.redis.config;
+
+import java.time.Duration;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Configuration
+public class CacheConfig {
+
+	private final RedisConnectionFactory redisConnectionFactory;
+	private final ObjectMapper objectMapper;
+
+	public CacheConfig(RedisConnectionFactory redisConnectionFactory, ObjectMapper objectMapper) {
+		this.redisConnectionFactory = redisConnectionFactory;
+		this.objectMapper = objectMapper;
+	}
+
+	@Bean
+	public CacheManager redisCacheManager() {
+		RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration
+			.defaultCacheConfig()
+			.disableCachingNullValues()
+			.serializeKeysWith(
+				RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+			.serializeValuesWith(
+				RedisSerializationContext.SerializationPair.fromSerializer(
+					new GenericJackson2JsonRedisSerializer(objectMapper)))
+			.entryTtl(Duration.ofMinutes(5L));
+
+		return RedisCacheManager.RedisCacheManagerBuilder
+			.fromConnectionFactory(redisConnectionFactory)
+			.cacheDefaults(redisCacheConfiguration)
+			.build();
+	}
 }

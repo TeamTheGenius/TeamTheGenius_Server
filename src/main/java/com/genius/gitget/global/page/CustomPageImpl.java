3cb16ac47bc10a952f81bbfb1c4663210d2b16fc
DoHyung Kim <neo5188@gmail.com>
2024-08-26 15:19:19 +0900

    feat: Pageble 파라미터가 들어오는 컨트롤러에 LimitedSizePagination 어노테이션 적용 (#255)

---
+++ src/main/java/com/genius/gitget/global/page/CustomPageImpl.java
@@ -0,0 +1,42 @@
+package com.genius.gitget.global.page;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CustomPageImpl<T> extends PageImpl<T> {
+
+	@JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+	public CustomPageImpl(@JsonProperty("content") List<T> content, @JsonProperty("number") int number,
+		@JsonProperty("size") int size,
+		@JsonProperty("totalElements") Long totalElements, @JsonProperty("pageable") JsonNode pageable,
+		@JsonProperty("last") boolean last,
+		@JsonProperty("totalPages") int totalPages, @JsonProperty("sort") JsonNode sort,
+		@JsonProperty("first") boolean first,
+		@JsonProperty("numberOfElements") int numberOfElements) {
+		super(content, PageRequest.of(number, size), totalElements);
+	}
+
+	public CustomPageImpl(List<T> content, Pageable pageable, Long total) {
+		super(content, pageable, total);
+	}
+
+	public CustomPageImpl(List<T> content) {
+		super(content);
+	}
+
+	public CustomPageImpl() {
+		super(new ArrayList<T>());
+	}
+}
+
